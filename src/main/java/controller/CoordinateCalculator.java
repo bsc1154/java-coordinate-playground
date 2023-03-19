4578179c525d6bfc172dcf7235aaeb3a66948bf8
khl1154 <khl1154@naver.com>
2023-03-19 20:47:10 +0900

    feat : step1, step2, step3 commit

---
+++ src/main/java/controller/CoordinateCalculator.java
@@ -0,0 +1,14 @@
+package controller;
+
+
+import model.Figure;
+import view.InputView;
+import view.OutputView;
+
+public class CoordinateCalculator {
+    public void run() {
+        Figure figure = InputView.inputCoordinates();
+        OutputView.showCoordinatePlane(figure);
+        OutputView.showArea(figure);
+    }
+}
